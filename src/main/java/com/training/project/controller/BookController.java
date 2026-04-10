7b77d9233e2c5e39dba0b6a1ac22ac8edd687aca
npmcatipon <npmcatipon@bpi.com.ph>
2026-04-11 02:20:50 +0800

    refactor: ApiResponse to accommodate success and error. feat: added delete and update book

--- src/main/java/com/training/project/controller/BookController.java
+++ src/main/java/com/training/project/controller/BookController.java
@@ -6,19 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.training.project.dto.BookDTO;
 import com.training.project.entity.Book;
 import com.training.project.exception.ApiResponse;
+import com.training.project.exception.BookIdNotFoundException;
 import com.training.project.service.BookService;
 
 import jakarta.validation.Valid;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -33,33 +35,36 @@ public class BookController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Book>> readBook(){
-        return ResponseEntity
-                .status(HttpStatus.OK.value())
-                .body(bookService.readBook());
+    public ResponseEntity<ApiResponse<List<Book>>> readBook(){
+        List<Book> books = bookService.readBook();
+        if (books.isEmpty()) {
+            throw new BookIdNotFoundException();
+        }
+        return ResponseEntity.ok(ApiResponse.success("Success", books));
     }
     
     @PostMapping
-    public ResponseEntity<ApiResponse> createBook(@Valid @RequestBody BookDTO bookDto) {
+    public ResponseEntity<ApiResponse<BookDTO>> createBook(@Valid @RequestBody BookDTO bookDto) {
         BookDTO newBookDto = bookService.createBook(bookDto);
-        return ResponseEntity
-                .status(HttpStatus.CREATED.value())
-                .body(
-                    ApiResponse.of(HttpStatus.CREATED.value(), 
-                    "Successfully created a book. " + 
-                    newBookDto.toString())
-                );
+        return ResponseEntity.ok(ApiResponse.success("Success", newBookDto));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse> deleteBook(@PathVariable Long id) {
-        return ResponseEntity
-                .status(HttpStatus.OK.value())
-                .body(
-                    ApiResponse.of(
-                        HttpStatus.OK.value(), 
-                    "Successfully deleted " +
-                    bookService.deleteBook(id)));
+    public ResponseEntity<ApiResponse<BookDTO>> deleteBook(@PathVariable Long id) {
+        return ResponseEntity.ok(ApiResponse.success(
+            "Successfully deleted book.", 
+            bookService.deleteBook(id)));
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<BookDTO>> updateBook(
+        @PathVariable Long id,
+        @Valid @RequestBody BookDTO bookDto
+    ) {
+        return ResponseEntity.ok(ApiResponse.success(
+            "Successfully Updated the Book.", 
+            bookService.updateBook(id, bookDto)));
     }
     
 
